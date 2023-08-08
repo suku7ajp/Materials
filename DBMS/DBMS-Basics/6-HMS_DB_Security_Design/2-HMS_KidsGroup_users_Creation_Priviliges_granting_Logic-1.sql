08269ae6a4d6b390cfe712d9aa86f77b92fbe34b
sv <sv@email.com>
2023-08-08 14:00:27 +0530

    servlet-4v DBMS-material-2v

---
+++ DBMS/DBMS-Basics/6-HMS_DB_Security_Design/2-HMS_KidsGroup_users_Creation_Priviliges_granting_Logic-1.sql
@@ -0,0 +1,12 @@
+create user suma identified by suma;
+grant select on grocery_item to suma;
+grant select on school_fee_payment to suma;
+create user suha identified by suha;
+grant select on grocery_item to suha;
+grant select on school_fee_payment to suha;
+grant create session to suma;
+grant create session to suha;
+
+
+
+
